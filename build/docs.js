8bbef49fd0a4a82997de85c0824af4f7877a6052
Even Torset <eventorset@gmail.com>
2024-04-01 01:34:09 +0200

    Fixed the functions documentation not showing up on the website. Thank you, Cloudflare, for not letting me disable the functions folder feature or configure it to use a different folder. It's really great that this is a thing, definitely has a positive impact on the world :)

---
+++ build/docs.js
@@ -0,0 +1,36 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import zlib from 'node:zlib'
+
+async function* getFiles(dir) {
+  const dirents = await fs.readdir(dir, { withFileTypes: true })
+  for (const dirent of dirents) {
+    const res = path.resolve(dir, dirent.name)
+    if (dirent.isDirectory()) {
+      yield* getFiles(res)
+    } else {
+      yield res
+    }
+  }
+}
+
+await fs.rename('docs/functions', 'docs/funcs')
+
+for await (const filePath of getFiles('docs')) {
+  if (filePath.endsWith('.html')) {
+    const content = await fs.readFile(filePath, 'utf-8')
+    await fs.writeFile(filePath, content.replace(/href="functions/g, 'href="func'))
+  }
+}
+
+{
+  const content = await fs.readFile('docs/assets/navigation.js', 'utf-8')
+  const list = JSON.parse(zlib.gunzipSync(Buffer.from(content.match(/;base64,(.*)"/)[1], 'base64')).toString('utf-8'))
+  for (const e of list) {
+    e.path = e.path.replace(/^functions/, 'funcs')
+  }
+  await fs.writeFile(
+    'docs/assets/navigation.js',
+    content.replace(/;base64,(.*)"/, ';base64,'+zlib.gzipSync(Buffer.from(JSON.stringify(list))).toString('base64')+'"')
+  )
+}
